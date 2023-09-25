95a31ff315a63230554071ce2dec6f0ff2dbee17
Tanmay-Shinde <tanmay_shinde@yahoo.com>
2023-09-25 16:13:13 -0400

    GetGradeUseCase: gradeDB made non-static

--- src/main/java/use_case/GetGradeUseCase.java
+++ src/main/java/use_case/GetGradeUseCase.java
@@ -4,13 +4,13 @@ import entity.Grade;
 import api.GradeDB;
 
 public final class GetGradeUseCase {
-    private static GradeDB gradeDB = null;
+    private final GradeDB gradeDB;
 
     public GetGradeUseCase(GradeDB gradeDB) {
-        GetGradeUseCase.gradeDB = gradeDB;
+        this.gradeDB = gradeDB;
     }
 
-    public static Grade getGrade(String utorid, String course) {
-        return gradeDB.getGrade(utorid, course);
+    public Grade getGrade(String utorid, String course) {
+        return this.gradeDB.getGrade(utorid, course);
     }
 }
